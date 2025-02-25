06641850de03818daab4a7186a37d8af4aeb5cd0
Harsha <harshavardhankond@gmail.com>
2025-02-25 11:54:38 +0530

    added

--- newtask/src/App.js
+++ newtask/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Seacrhfilter from './search/Searchfilter'
 
+
 const App = () => {
   return (
     <div>
-      <Seacrhfilter/>
+    <Seacrhfilter/>
     </div>
   )
 }
